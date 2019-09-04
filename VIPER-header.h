e86417d75220c1c8c1e831717ff65086bbffe822
iTBoyer <itboyer@qq.com>
2019-09-04 13:34:25 +0800

    文档整理

---
+++ VIPER-header.h
@@ -0,0 +1,28 @@
+//
+//  umbrella-header.h
+//  VIPER TODO
+//
+//  Created by admin on 2019/9/4.
+//  Copyright © 2019 Mutual Mobile. All rights reserved.
+//
+
+#ifndef umbrella_header_h
+#define umbrella_header_h
+#import "VTDAppDependencies.h"
+
+#import "VTDRootWireframe.h"
+
+#import "VTDCoreDataStore.h"
+#import "VTDDeviceClock.h"
+
+#import "VTDListDataManager.h"
+#import "VTDListInteractor.h"
+#import "VTDListPresenter.h"
+#import "VTDListWireframe.h"
+
+#import "VTDAddDataManager.h"
+#import "VTDAddInteractor.h"
+#import "VTDAddPresenter.h"
+#import "VTDAddWireframe.h"
+
+#endif /* umbrella_header_h */
